2558c997af831da61ed916c5287a3818751b6c9c
makwana hitesh <makwanah538@gmail.com>
2025-11-27 16:46:08 +0530

    cicd

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0fa3fe0fa7920f68e"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "DevInstance"
+    }
+}
